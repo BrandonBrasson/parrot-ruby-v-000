9fbd6fdaeea62084bd7f65f6b5f06cfc126b794c
Brandon Brasson <rocksong622@gmail.com>
2019-01-04 21:40:29 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,12 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-# method should accept an argument of a string and both `#puts` out that string *and* return the string at the end of the method. 
+# method should accept an argument of a string and both `#puts` out that string *and* return the string at the end of the method.
 
 puts "Squawk"
 
 def parrot()
 puts "Pretty bird"
 end
-
-
-
